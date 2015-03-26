8819e76327fb69ce326cfec6ea45ba07a2bffd8e
flogo1982 <florian.goussin@gmail.com>
2015-03-25 21:27:07 -0600

    integration wip: create a style.css for the theme and work on functions.php

---
+++ wp-content/themes/aandbae-splash/functions.php
@@ -0,0 +1,64 @@
+<?php
+
+// specify the site url and the home url
+update_option('siteurl','http://aandbae.com');
+update_option('home','http://aandbae.com');
+
+
+/**
+ * load the css files
+ */
+function aandbae_load_css() {
+	wp_enqueue_style('bootstrap_css', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css');
+	wp_enqueue_style('vanilla_css', get_template_directory_uri() . '/css/vanilla.css');
+	wp_enqueue_style('main_css', get_template_directory_uri() . 'css/main.css');
+}
+add_action('wp_enqueue_scripts', 'aandbae_load_css');
+
+
+/**
+ * load the js files
+ */
+function aandbae_load_js() {
+
+	// old browsers compatibility
+	global $wp_scripts;
+	wp_register_script('html5_shiv', '/js/html5shiv.js', '', '', false);
+	wp_register_script('respond_js', 'https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js', '', '', false);
+	$wp_scripts->add_data('html5_shiv', 'conditional', 'lt IE 9');
+	$wp_scripts->add_data('respond_js', 'conditional', 'lt IE 9');
+
+	wp_enqueue_script('bootstrap_js', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/js/bootstrap.min.js', array('jquery'), '3', true);
+}
+add_action('wp_enqueue_scripts', 'aandbae_load_js');
+
+/**
+ * remove the admin Word Press bar
+ */
+function aandbae_remove_admin_bar() {
+	if (!current_user_can('administrator') && !is_admin()) {
+		show_admin_bar(false);
+	}
+}
+add_action('after_setup_theme', 'aandbae_remove_admin_bar');
+
+
+/*
+ * Widget creation
+ */
+function aandbae_create_widget( $name, $id, $before_widget, $after_widget, $before_title, $after_title, $description ) {
+
+	$args = array(
+		'name'          => __( $name ),
+		'id'            => $id,
+		'description'   => __( $description ),
+		'before_widget' => $before_widget,
+		'after_widget'  => $after_widget,
+		'before_title'  => $before_title,
+		'after_title'   => $after_title
+	);
+
+	register_sidebar( $args );
+}
+// change main text
+aandbae_create_widget( 'Main Text', 'main-text', '', '', '', '', 'Widget for changing the Main Text.');
---
+++ wp-content/themes/aandbae-splash/index.php
@@ -0,0 +1,42 @@
+
+<div class="container-fluid">
+	<div class="row child-pad0">
+	  <div class="col-xs-4">
+		 <img src="images/desk-1.jpg" alt="desk" class="img-responsive">
+	  </div>
+	  <div class="col-xs-4">
+		 <img src="images/dzia-5.jpg" alt="dzia" class="img-responsive">
+	  </div>
+	  <div class="col-xs-4">
+		 <img src="images/Shylight-Rijks-11.jpg" alt="shylight" class="img-responsive">
+	  </div>
+	</div>
+</div>
+
+<div class="container-fluid even-margin mt60">
+	<div class="row">
+	  <div class="col-sm-12 t-center">
+		 <h1>A &amp; Bae Designs</h1>
+
+		 <p>Meditation readymade cray migas, pop-up paleo pug cronut forage. <br>
+		 Gentrify distillery DIY, photo booth paleo keytar before they sold out whatever meh cliche organic <br>
+		 swag selvage narwhal. Pop-up cardigan cronut, kogi asymmetrical shabby chic artisan slow-carb PBR&B. <br>
+		 Hashtag meditation Odd Future Shoreditch crucifix distillery seitan put a bird on it.</p>
+	  </div>
+	</div>
+</div>
+
+<div class="container-fluid even-margin mt60">
+	<div class="row child-pad0">
+	  <div class="col-sm-6 t-center">
+			<a href="http://jordyn.aandbae.com/">
+				<img src="images/designer-placeholder-hires-1.jpg" alt="Jordyn" class="img-responsive iblock">
+			</a>
+	  </div>
+	  <div class="col-sm-6 t-center">
+			<a href="http://jordyn.aandbae.com/">
+				<img src="images/designer-placeholder-hires-2.jpg" alt="designer2" class="img-responsive iblock">
+		  </a>
+	  </div>
+	</div>
+</div>
